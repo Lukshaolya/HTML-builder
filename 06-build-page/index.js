cd22900c9c33a78307410300bb1b24cb92e1ea04
Lukshaolya <lukshaolya98@gmail.com>
2023-05-08 13:34:35 +0300

    feat: done 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,6 +2,9 @@ let fs = require('fs');
 const path = require('path');
 
 let result = '';
+const old = path.join(__dirname, 'assets');
+const newOld = path.join(__dirname, 'project-dist','assets');
+console.log(newOld)
 
 async function getInfo() {
     let stylesFile = path.join(__dirname, 'styles');
@@ -14,8 +17,12 @@ async function getInfo() {
         result += '\n';
       }
     }
+
+    createFolder();
+    readTemplate() ;
     collectStyles();
-    createFolder()
+    createFolderAssets();
+    myCopy(old, newOld);
   }
   
   async function createFolder() {
@@ -25,6 +32,16 @@ async function getInfo() {
             throw err;
           }
           console.log('Folder exist')
+    })
+    await promise;
+  }
+  async function createFolderAssets() {
+    let projectFile = path.join(__dirname, 'project-dist', 'assets');
+    const promise = fs.promises.mkdir(projectFile,(err)=>{
+        if (err) {
+            throw err;
+          }
+        console.log('Folder exist')
     })
     await promise;
   }
@@ -39,4 +56,67 @@ async function getInfo() {
     await promise;
   }
   
+ 
+  
+
+  async function clearDir(newOld) {
+    const arrOfobj = await fs.promises.readdir(newOld, {withFileTypes: true});
+    for (let obj of arrOfobj) {
+      if (obj.isFile()) {
+        await fs.promises.rm(path.join(newOld, obj.name));
+      }
+    }
+  }
+  
+  async function myCopy(old, newOld) {
+    const copyDir = await fs.promises.mkdir(newOld, { recursive: true });
+    await clearDir(newOld);
+    const arrOfobj = await fs.promises.readdir(old, { withFileTypes: true });
+    for (let obj of arrOfobj) {
+      if (obj.isFile()) {
+        const pathOrgFile = path.join(old, obj.name);
+        const pathCopyFile = path.join(newOld, obj.name);
+        await fs.promises.copyFile(pathOrgFile, pathCopyFile);
+      }
+      else if(obj.isDirectory()) {
+        myCopy(path.join(old, obj.name), path.join(newOld, obj.name));
+      }
+    }
+  }
+  async function readTemplate() {
+    const pathTmp = path.join(__dirname, 'template.html');
+    const contents = await fs.promises.readFile(pathTmp, { encoding: 'utf8' });
+    changeData(contents);
+  }
+  
+  async function foo(path) {
+    const data = await fs.promises.readFile(path, { encoding: 'utf8' });
+    return data;
+  }
+  
+  async function changeData(data) {
+    const arrHTMLfiles = [];
+    const pathComponents = path.join(__dirname, 'components');
+    const arrDirent = await fs.promises.readdir(pathComponents, {withFileTypes: true});
+    for(let item of arrDirent) {
+      let pathFile = path.join(pathComponents, item.name);
+      const data = await foo(pathFile);
+      arrHTMLfiles.push({
+        name: item.name = path.basename(pathFile, path.extname(pathFile)),
+        path: pathFile,
+        content: data,
+      });
+    }
+    arrHTMLfiles.forEach(obj => {
+      data = data.replace(`{{${obj.name}}}`, obj.content);
+    });
+    writeHTML(data);
+}
+  
+async function writeHTML(data) {
+    let pathHTML = path.join(__dirname, 'project-dist', 'index.html');
+    const promise = fs.promises.writeFile(pathHTML, data);
+    await promise;
+  }
+
   getInfo();
