1f966d0ae8cb7e259bc16556ddf0c38a007a5d5c
Lukshaolya <lukshaolya98@gmail.com>
2023-05-08 13:04:22 +0300

    feat: done 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+let fs = require('fs');
+const path = require('path');
+
+let result = '';
+
+async function getInfo() {
+    let stylesFile = path.join(__dirname, 'styles');
+    const data = await fs.promises.readdir(stylesFile, {withFileTypes: true});
+    for (let item of data) {
+      const pathFile = path.join(__dirname, 'styles', item.name);
+      if(item.isFile() && (path.extname(pathFile) === '.css')) {
+        const contents = await fs.promises.readFile(pathFile, { encoding: 'utf8' });
+        result += contents;
+        result += '\n';
+      }
+    }
+    collectStyles();
+  }
+  
+  async function collectStyles() {
+    let bundleFile = path.join(__dirname, 'project-dist', 'bundle.css');
+    const promise = fs.promises.writeFile(bundleFile, res,(err)=>{
+        if (err) {
+            throw err;
+          }
+          console.log('File exist')
+    })
+    await promise;
+  }
+  
+  getInfo();
